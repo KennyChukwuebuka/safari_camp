05603dc765f1067d87277f7eb1713024dc03a4e1
kennyChukwuebuka <kenkinado@gmail>
2024-02-29 22:32:02 +0100

    styling of pages

--- v1/app.js
+++ v1/app.js
@@ -8,8 +8,17 @@ app.set('view engine', 'ejs');
 
 let campgrounds = [
     {name: 'Salmon Creek', image: 'https://www.elacampground.com/wp-content/uploads/2019/06/Ela-Campground-87.jpg'},
-    {name: 'Granite Hill', image: 'https://www.shutterstock.com/shutterstock/photos/1956989740/display_1500/stock-photo-travel-trailer-camping-in-the-woods-at-a-campground-1956989740.jpg'},
-    {name: 'Mountain Goat', image: 'https://www.shutterstock.com/shutterstock/photos/1411568588/display_1500/stock-photo-kids-playground-in-the-park-surrounded-by-green-trees-1411568588.jpg'},
+    {name: 'Granite Hill', image: 'https://www.elacampground.com/wp-content/uploads/2019/06/Ela-Campground-87.jpg'},
+    {name: 'Mountain Goat', image: 'https://www.elacampground.com/wp-content/uploads/2019/06/Ela-Campground-87.jpg'},
+    {name: 'Salmon Creek', image: 'https://www.elacampground.com/wp-content/uploads/2019/06/Ela-Campground-87.jpg'},
+    {name: 'Granite Hill', image: 'https://www.elacampground.com/wp-content/uploads/2019/06/Ela-Campground-87.jpg'},
+    {name: 'Mountain Goat', image: 'https://www.elacampground.com/wp-content/uploads/2019/06/Ela-Campground-87.jpg'},
+    {name: 'Salmon Creek', image: 'https://www.elacampground.com/wp-content/uploads/2019/06/Ela-Campground-87.jpg'},
+    {name: 'Granite Hill', image: 'https://www.elacampground.com/wp-content/uploads/2019/06/Ela-Campground-87.jpg'},
+    {name: 'Mountain Goat', image: 'https://www.elacampground.com/wp-content/uploads/2019/06/Ela-Campground-87.jpg'},
+    {name: 'Salmon Creek', image: 'https://www.elacampground.com/wp-content/uploads/2019/06/Ela-Campground-87.jpg'},
+    {name: 'Granite Hill', image: 'https://www.elacampground.com/wp-content/uploads/2019/06/Ela-Campground-87.jpg'},
+    {name: 'Mountain Goat', image: 'https://www.elacampground.com/wp-content/uploads/2019/06/Ela-Campground-87.jpg'},
 
 ];
 
